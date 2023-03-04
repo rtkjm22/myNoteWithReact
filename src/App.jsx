ac00c5c791f5a64c78d84e7ad46c18497db3d4b4
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2023-03-04 19:58:17 +0900

    ADD:サイドバーの新規追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
 import './App.css'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+import uuid from "react-uuid";
 
 function App() {
+  const [notes, setNotes] = useState([])
+  const [activeNote, setActiveNote] = useState(false)
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: '新しいノート',
+      content: '新しいノートの内容',
+      modDate: Date.now()
+    }
+
+    setNotes([...notes, newNote])
+  }
+
+  const onDeleteNote = (id) => {
+    const newNotes = [...notes].filter((note) => note.id !== id)
+    setNotes(newNotes)
+  }
 
   return (
-    <div className="App">
-      
+    <div className="App w-full h-screen overflow-hidden text-base flex bg-blue-50">
+      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote} />
+      <Main />
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Sidebar({
+  onAddNote,
+  notes,
+  onDeleteNote,
+  activeNote,
+  setActiveNote
+}) {
+  return (
+    <div className="w-[30%] h-screen border-r">
+      <div className="flex justify-between p-6">
+        <h1>ノート</h1>
+        <button
+          className="text-blue-500 text-base cursor-pointer"
+          onClick={onAddNote}
+        >
+          追加
+        </button>
+      </div>
+      <div className="h-[100vh - 78px] overflow-y-scroll">
+        {notes.map(({ id, title, content, modDate }) => (
+          <div
+            className={`p-6 hover:bg-gray-200 transition-all ${activeNote === id && "bg-gray-200"}`}
+            key={id}
+            onClick={() => setActiveNote(id)}
+          >
+            <div className="flex justify-between">
+              <strong>{title}</strong>
+              <button onClick={() => onDeleteNote(id)}>削除</button>
+            </div>
+            <p className="my-2.5">{content}</p>
+            <small className="text-gray-400">
+              最後の修正日：
+              {new Date(modDate).toLocaleDateString('ja-JP', {
+                hour: '2-digit',
+                minute: '2-digit'
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
